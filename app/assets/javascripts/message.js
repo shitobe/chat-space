bff06e8c4c14fe820e02866d8e2e73d513a4c6ab
iemoto <57856680+iemoto@users.noreply.github.com>
2019-12-16 21:28:27 +0900

    html add

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,45 @@
 $(function(){
+  function buildHTML(message){
+    if (message.image) {
+      var html = `<div class="chat-main__message-list-board__lists">
+      <div class="chat-main__message-list-board__lists__list">
+        <div class="chat-main__message-list-board__lists__list__input-to">
+          <div class="chat-main__message-list-board__lists__list__input-to--name">
+            ${message.user.name}
+          </div>
+          <div class="chat-main__message-list-board__lists__list__input-to--date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="chat-main__message-list-board__lists__list__message">
+          <p class="chat-main__message-list-board__lists__list__message__content">
+            ${message.content}
+          </p>
+          <img class="chat-main__message-list-board__lists__list__message__image" src= alt="Images">
+        </div>
+      </div>
+    </div>` 
+    } else {
+      var html = `<div class="chat-main__message-list-board__lists">
+                    <div class="chat-main__message-list-board__lists__list">
+                      <div class="chat-main__message-list-board__lists__list__input-to">
+                        <div class="chat-main__message-list-board__lists__list__input-to--name">
+                          ${message.user.name}
+                        </div>
+                        <div class="chat-main__message-list-board__lists__list__input-to--date">
+                          ${message.created_at}
+                        </div>
+                      </div>
+                      <div class="chat-main__message-list-board__lists__list__message">
+                        <p class="chat-main__message-list-board__lists__list__message__content">
+                          ${message.content}
+                        </p>
+                      </div>
+                    </div>
+                  </div>` 
+    }
+    return html
+  }
   $('#new_message').on('submit' , function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -12,10 +53,13 @@ $(function(){
       contentType: false
     })
     .done(function(message){
-      console.log(message)
+      var html = buildHTML(message);
+      $('.chat-main__message-list-board').append(html);
+      $('#message_content').val('');
+
     })
     .fail(function(){
-      error 'エラー'
+      alert("メッセージ送信に失敗しました");
     })
   })
 });
